0044bc9669b6d91d6e1e934d701ee34b17d708d8
Abdul Rehman <abdulr568programming@outlook.com>
2023-11-04 10:47:23 +0000

    Removed wrapper div when start button pressed

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,8 +1,10 @@
 let startButton = document.getElementById('start');
+let wrapper = document.getElementsByClassName('wrapper')[0];
 
 startButton.addEventListener('click', function () {
     var timeLeft = 75;
     var timeInterval = setInterval(function () {
+        wrapper.classList.add("hide");
         document.getElementById('time').innerText = timeLeft;
         timeLeft--;
         if (timeLeft === 0) {
