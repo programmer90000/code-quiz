1921bc3af44179e4a4fffba46632e81048e601a1
Abdul Rehman <abdulr568programming@outlook.com>
2023-11-06 11:42:44 +0000

    Added questions

---
+++ assets/js/questions.js
@@ -0,0 +1,108 @@
+let questions = [
+    {
+        question: "What is the difference bwtween var, let and const",
+        possibleAnswers: [
+            "var declares variables globally. let defines variables locally. const defines variables which can't be changed",
+            "let declares variables globally. var defines variables locally. const defines variables which can't be changed",
+            "const declares variables globally. let defines variables locally. var defines variables which can't be changed",
+            "var declares variables globally. const defines variables locally. let defines variables which can't be changed",
+            "const declares variables globally. var defines variables locally. let defines variables which can't be changed",
+        ],
+        correctAnswer: 0,
+    },
+    {
+        question: "What is the difference between null and undefined",
+        possibleAnswers: [
+            "null is used to represent an uninitialized variable. undefined is used to represent a missing value",
+            "undefined is used to represent an uninitialized variable. null is used to represent a missing value",
+            "null is used for a variable with a value of 0. undefined is used to represent a variable which has not been defined",
+        ],
+        correctAnswer: 1,
+    },
+    {
+        question: "What is the difference between == and ===",
+        possibleAnswers: [
+            "== is used for strict equality. while === is used for loose equality",
+            "=== is used for strict equality, while == is used for loose equality",
+            "== is used for comparison, while === is used for assignment",
+            "=== is used for comparison, while == is used for assignment",
+        ],
+        correctAnswer: 1,
+    },
+    {
+        question: "What is the difference between a Arrow Function and a Regular Function",
+        possibleAnswers: [
+            "Arrow functions are anonymous and cannot be named, while regular functions can be named",
+            "Arrow functions cannot be used as constructors, while regular functions can",
+            "Arrow functions do not have their own arguments object, while regular functions do",
+            "Arrow functions do not have their own this value, while regular functions do",
+        ],
+        correctAnswer: 3,
+    },
+    {
+        question: "What is the difference between the map and filter Array methods",
+        possibleAnswers: [
+            "map is used to create a new array with the same number of elements as the original array, while filter is used to create a new array with fewer elements than the original array",
+            "filter is used to create a new array with the same number of elements as the original array, while map is used to create a new array with fewer elements than the original array",
+            "map is used to transform the elements of an array, while filter is used to select elements from an array based on a condition",
+            "map is used to modify the elements of an array, while filter is used to remove elements from an array",
+            "filter is used to modify the elements of an array, while map is used to remove elements from an array",
+        ],
+        correctAnswer: 2,
+    },
+    {   
+        question: "What is the difference between a local and a global variable",
+        possibleAnswers: [
+            "A local variable is declared inside a function, while a global variable is declared outside a function",
+            "A local variable is declared outside a function, while a global variable is declared inside a function",
+            "A local variable is accessible from anywhere in the program, while a global variable is only accessible from within the function where it is declared",
+            "A global variable is accessible from anywhere in the program, while a local variable is only accessible from within the function where it is declared",
+        ],
+        correctAnswer: 3,
+    },
+    {
+        question: "What is the difference between a parameter and an argument",
+        possibleAnswers: [
+            "A parameter is a variable in a method definition, while an argument is the actual value passed to the function when it is called",
+            "A parameter is a value that is passed to a function, while an argument is a variable that is defined in the function",
+            "A parameter is a variable that is defined in the function, while an argument is a value that is passed to the function",
+            "A parameter is a variable that is defined in the function, while an argument is a value that is returned by the function",
+            "A parameter is a value that is returned by the function, while an argument is a variable that is defined in the function",
+        ],
+        correctAnswer: 0,
+    },
+    {
+        question: "What is the difference between a class and an object",
+        possibleAnswers: [
+            "A class is a type of object, while an object is an instance of a class",
+            "A class is a blueprint for creating objects, while an object is an instance of a class",
+            "A class is a collection of objects, while an object is a single entity",
+            "A class is a function, while an object is a variable",
+            "A class is a constructor, while an object is an instance",
+        ],
+        correctAnswer: 1,
+    },
+    {
+        question: "What is the syntax for to check if the variable x is strictly equal to 5",
+        possibleAnswers: [
+            "if (x = 5) {}",
+            "if (x == 5) {}",
+            "if (x === 5) {}",
+            "if (x != 5) {}",
+            "if (x !== 5) {}",
+            "if x == 5 {}",
+        ],
+        correctAnswer: 2,
+    },
+    {
+        question: "What is the syntax for strict not equal to",
+        possibleAnswers: [
+            "=",
+            "==",
+            "===",
+            "!=",
+            "!==",
+        ],
+        correctAnswer: 4,
+    },
+];
