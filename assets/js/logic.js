ee6382656fae7d3097c5bd2911dbb4fe361fe6f7
Abdul Rehman <127865680+programmer90000@users.noreply.github.com>
2023-11-06 10:38:41 +0000

    Added function to shuffle the Array

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,6 +2,7 @@ let startButton = document.getElementById('start');
 let wrapper = document.getElementsByClassName('wrapper')[0];
 
 startButton.addEventListener('click', function () {
+    shuffleArray(questions);
     var timeLeft = 75;
     var timeInterval = setInterval(function () {
         wrapper.classList.add("hide");
@@ -13,3 +14,10 @@ startButton.addEventListener('click', function () {
         }
     },1000)
 })
+
+function shuffleArray (arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let j = Math.floor(Math.random() * i);
+        [arr[i-1], arr[j]] = [arr[j], arr[i-1]];
+    }
+}
