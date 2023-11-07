ce3fd3f63fd342099cb2d1dcfc06ad573b85dfd8
Abdul Rehman <abdulr568programming@outlook.com>
2023-11-07 12:09:11 +0000

    Ordered table by score

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -2,6 +2,8 @@ let newScore = JSON.parse(localStorage.getItem("scores"));
 
 console.log(newScore);
 
+newScore.sort((a, b) => b.score - a.score);
+
 for (let i = 0; i < newScore.length; i++) {
     let scoreElement = document.createElement("li");
     scoreElement.innerText = newScore[i].initials + " - " + newScore[i].score;
